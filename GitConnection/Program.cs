3362aad7f268d6d03e06c0a0fa95de533a6f53a0
Dhirendra Mohan <Dhirendra.mohan@zensar.com>
2021-01-25 13:00:46 +0530

    Add project files.

---
+++ GitConnection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting a Project with Git");
+            Console.ReadLine();
+        }
+    }
+}
